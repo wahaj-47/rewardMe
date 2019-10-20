833c475e5deb6ddebddfa3c44f040071253dd1bb
wahaj-47 <wahajhssn@gmail.com>
2019-10-20 17:28:40 +0500

    updated sql

--- sql/rewardme.sql
+++ sql/rewardme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2019 at 02:00 PM
+-- Generation Time: Oct 20, 2019 at 02:28 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -19,8 +19,46 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mh reward me`
+-- Database: `rewardme`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `hash` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `name`, `email`, `hash`) VALUES
+(0, 'Admin', 'admin@email.com', '827ccb0eea8a706c4c34a16891f84e7b');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `device_tokens`
+--
+
+CREATE TABLE `device_tokens` (
+  `user_id` int(11) NOT NULL,
+  `device_token` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `device_tokens`
+--
+
+INSERT INTO `device_tokens` (`user_id`, `device_token`) VALUES
+(18, 'ExponentPushToken[1fLOY9IyiV1Atx-T81dkRc]');
 
 -- --------------------------------------------------------
 
@@ -30,10 +68,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `logs` (
   `log_id` int(11) NOT NULL,
-  `timestamp` date NOT NULL,
+  `timestamp` date NOT NULL DEFAULT current_timestamp(),
   `value` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`log_id`, `timestamp`, `value`, `user_id`) VALUES
+(10, '2019-10-20', 25, 18),
+(11, '2019-10-20', 30, 18),
+(12, '2019-10-20', 35, 18),
+(13, '2019-10-20', 40, 18);
 
 -- --------------------------------------------------------
 
@@ -67,8 +115,23 @@ CREATE TABLE `personal_notice` (
 
 CREATE TABLE `revealed_slots` (
   `slot_id` int(11) NOT NULL,
+  `slot_value` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `revealed_slots`
+--
+
+INSERT INTO `revealed_slots` (`slot_id`, `slot_value`, `user_id`) VALUES
+(1, 0, 18),
+(2, 0, 18),
+(3, 0, 18),
+(4, 0, 18),
+(5, 0, 18),
+(6, 0, 18),
+(7, 0, 18),
+(8, 0, 18);
 
 -- --------------------------------------------------------
 
@@ -80,6 +143,22 @@ CREATE TABLE `slots` (
   `slot_id` int(11) NOT NULL,
   `slot_value` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `slots`
+--
+
+INSERT INTO `slots` (`slot_id`, `slot_value`) VALUES
+(1, 5),
+(2, 10),
+(3, 15),
+(4, 20),
+(5, 25),
+(6, 30),
+(7, 35),
+(8, 40),
+(9, 45),
+(10, 50);
 
 -- --------------------------------------------------------
 
@@ -95,8 +174,30 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `email`, `hash`) VALUES
+(18, 'Wahaj Hussain', 'wahajhssn@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(19, 'Hashir Tihami', 'hashirtihamipk@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(22, 'Waleed Hussain', 'waleedhssn@gmail.com', '25f9e794323b453885f5181f1b624d0b');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `device_tokens`
+--
+ALTER TABLE `device_tokens`
+  ADD UNIQUE KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `logs`
@@ -135,7 +236,8 @@ ALTER TABLE `slots`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -145,7 +247,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `notice`
@@ -157,17 +259,23 @@ ALTER TABLE `notice`
 -- AUTO_INCREMENT for table `slots`
 --
 ALTER TABLE `slots`
-  MODIFY `slot_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `slot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `device_tokens`
+--
+ALTER TABLE `device_tokens`
+  ADD CONSTRAINT `device_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `logs`
